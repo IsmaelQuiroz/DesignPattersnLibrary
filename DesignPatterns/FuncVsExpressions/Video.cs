fa0332128f676812a39b4e2cf5473ae6815f3423
Ismael Quiroz Santes <ismaelqzs@gmail.com>
2026-05-13 23:22:12 -0600

    FuncT Sample

---
+++ DesignPatterns/FuncVsExpressions/Video.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.FuncVsExpressions
+{
+    public class Video
+    {
+        public string Nombre { get; set; }
+       
+        public static void funcT()
+        {
+            var videos = new List<Video>
+            {
+                new Video { Nombre ="matrix"},
+                new Video { Nombre = "mad max"},
+                new Video { Nombre = "avatar"}
+            };
+
+            Func<Video, string> selector = (video) => "Pelicula:" + video.Nombre;
+
+            IEnumerable<string> videoTitulos = videos.Select(selector);
+            foreach(string titulos in videoTitulos)
+            {
+                Console.WriteLine(titulos);
+            }
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -4,26 +4,34 @@ using DesignPatterns.E01_RepositoryPattern;
 using DesignPatterns.E02_ISpecificationPattern;
 using DesignPatterns.E02_ISpecificationPattern.DemoE02;
 using DesignPatterns.E03_ISpecificationPattern.Demo;
+using DesignPatterns.FuncVsExpressions;
 using System.Linq;
+using DesignPatterns.FuncVsExpressions;
+
 
 //Execution of exercises
+
+//Func vs Expressions
+Video.funcT();
+
+
 /*----E03 Specification Pattern: Validación de Reglas de Negocion----------------------*/ 
 
-DemoSpecification.Run();
+//DemoSpecification.Run();
 
 
 /*--- E02 uso de la implementación del patron repository con Specification */
 
-DemoISpecification.correr();
+//DemoISpecification.correr();
 
 
 /*--------------Excercise01_PatronRepository - Implementation par avalidar fecha en rango de fechas */
 
-ISpecification<DateTime> validador = new ValidaFechaSpecification();
-DateTime fechaActual = DateTime.Now;
+//ISpecification<DateTime> validador = new ValidaFechaSpecification();
+//DateTime fechaActual = DateTime.Now;
 
-bool isvalid = validador.IsValidaFecha(fechaActual);
+//bool isvalid = validador.IsValidaFecha(fechaActual);
 
-Console.WriteLine($"\nFecha a Evaluar: {fechaActual}");
-Console.WriteLine($"Rango: 2001-01-01 hasta < 2026-05-01");
-Console.WriteLine($"¿Es válida? {isvalid}");
+//Console.WriteLine($"\nFecha a Evaluar: {fechaActual}");
+//Console.WriteLine($"Rango: 2001-01-01 hasta < 2026-05-01");
+//Console.WriteLine($"¿Es válida? {isvalid}");
